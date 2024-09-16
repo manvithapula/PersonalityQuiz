f2315a6c939d5f4aa4df9d4a5cd745f2553d263d
majorwinger1316 <akshatdutt1316@gmail.com>
2024-09-16 10:27:04 +0530

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by admin33 on 16/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
